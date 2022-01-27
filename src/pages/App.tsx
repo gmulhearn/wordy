a889128e2b4460c8967db4c042e229d69c00480e
gmulhearn <immpumpz@gmail.com>
2022-01-27 19:09:52 +1000

    reactive better

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -1,8 +1,7 @@
 import React from 'react';
 import { Box } from "@mui/material"
-import { makeStyles } from "@mui/styles"
 import { Route, Routes } from 'react-router';
-import Main from './main/main';
+import Main from './main/Main';
 
 const styles = {
   root: {
--- src/pages/main/main.tsx
+++ src/pages/main/main.tsx
@@ -6,13 +6,18 @@ import WordGrid, { LetterState } from '../../components/WordGrid';
 import { emptyGrid, LetterGridProcessor } from '../../core/LetterGridCore';
 
 const styles = {
+    title: {
+        fontWeight: "bolder",
+        textAlign: "center",
+        fontSize: "32px"
+    },
     main: {
         // border: 1,
-        height: "75%"
+        height: "100%"
     },
     keyboard: {
         // border: 1,
-        height: "25%"
+        // height: "30%"
     },
 }
 
@@ -32,14 +37,17 @@ const Main = () => {
     }
 
     return (
-        <>
+        <Box display="flex" justifyContent="space-between" flexDirection="column" sx={{minHeight:"100%"}}>
+            <Typography sx={styles.title}>
+                WORDLE2
+            </Typography>
             <Box sx={styles.main} display="flex" alignItems="center" justifyContent="center">
                 <WordGrid letterGrid={letterGrid} />
             </Box>
             <Box sx={styles.keyboard}>
                 <Keyboard onKeyboardInput={handleKeyboardInput} />
             </Box>
-        </>
+        </Box>
 
     );
 };
