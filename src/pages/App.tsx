f9872c65ad85dc2e7ffc8b84eccc02f0be6b10ca
gmulhearn <immpumpz@gmail.com>
2022-01-27 20:39:41 +1000

    practice mode

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -1,7 +1,11 @@
-import React from 'react';
-import { Box } from "@mui/material"
-import { Route, Routes } from 'react-router';
+import React, { useCallback } from 'react';
+import { AppBar, Box, Button, Typography } from "@mui/material"
+import { Route, Routes, useNavigate } from 'react-router';
 import Main from './main/main';
+import Practice from './practice/practice';
+import FitnessCenterIcon from '@mui/icons-material/FitnessCenter';
+import HomeIcon from '@mui/icons-material/Home';
+import LeaderboardIcon from '@mui/icons-material/Leaderboard';
 
 const styles = {
   root: {
@@ -13,11 +17,38 @@ const styles = {
 
 function App() {
 
+  const navigate = useNavigate();
+  const navHome = useCallback(() => navigate('/', { replace: true }), [navigate]);
+  const navPractice = useCallback(() => navigate('/practice', { replace: true }), [navigate]);
+  const navLeaderboard = useCallback(() => navigate('/leaderboard', { replace: true }), [navigate]);
+
+
   return (
     <Box sx={{ minHeight: "100%" }} display="flex" justifyContent="center">
+      <AppBar>
+        <Box display="flex" justifyContent="space-between" sx={{ padding: "0.5em" }} >
+          <Typography sx={{ fontWeight: "bold", fontSize: "24px" }}>
+            WORDLE2
+          </Typography>
+          <Box display="flex">
+            <Button onClick={navHome}>
+              <HomeIcon />
+            </Button>
+            <Button onClick={navPractice}>
+              <FitnessCenterIcon />
+            </Button>
+            <Button onClick={navLeaderboard}>
+              <LeaderboardIcon />
+            </Button>
+          </Box>
+
+        </Box>
+
+      </AppBar>
       <Box sx={styles.root} display="flex" flexDirection="column">
         <Routes>
           <Route path="/" element={<Main />} />
+          <Route path="/practice" element={<Practice />} />
         </Routes>
       </Box>
     </Box>
--- src/pages/practice/practice.tsx
+++ src/pages/practice/practice.tsx
@@ -1,15 +1,14 @@
-import { Box, Grid, Paper, Typography } from '@mui/material';
-import { makeStyles } from '@mui/styles';
+import { Box, Button, Dialog, Grid, IconButton, Paper, Typography } from '@mui/material';
 import React, { useState } from 'react';
 import Keyboard from '../../components/Keyboard';
-import WordGrid, { LetterState } from '../../components/WordGrid';
+import WordGrid from '../../components/WordGrid';
 import { emptyGrid, LetterGridProcessor } from '../../core/LetterGridCore';
 import words from '../../res/5letterwords.json'
+import FitnessCenterIcon from '@mui/icons-material/FitnessCenter';
 
 const styles = {
     title: {
         fontWeight: "bolder",
-        textAlign: "center",
         fontSize: "32px"
     },
     main: {
@@ -23,19 +22,19 @@ const styles = {
 }
 
 const getTodaysWord = (): string => {
-    const word = words.at(Math.floor(Math.random() * words.length-1))  || "UNDEF"
+    const word = words.at(Math.floor(Math.random() * words.length - 1)) || "UNDEF"
     return word.toUpperCase()
 }
 
-const Main = () => {
+const Practice = () => {
 
     const [letterGrid, setLetterGrid] = useState(emptyGrid);
+
+    const [revealWordOpen, setRevealWordOpen] = useState(false);
 
     const todaysWord = getTodaysWord()
     const [letterGridProcessor, setLetterGridProcessor] = useState(new LetterGridProcessor(todaysWord))
 
-
-    
     const handleKeyboardInput = (input: string) => {
         console.log(input)
         const newGrid = letterGridProcessor.processInput(input)
@@ -43,7 +42,21 @@ const Main = () => {
     }
 
     return (
-        <Box display="flex" justifyContent="space-between" flexDirection="column" sx={{minHeight:"100%"}}>
+        <Box display="flex" justifyContent="space-between" flexDirection="column" sx={{ minHeight: "100%" }}>
+            <Dialog open={revealWordOpen} onClose={() => { setRevealWordOpen(false) }}>
+                <Typography sx={{marginInline: "2em", marginBlock: "1em"}}>
+                    {letterGridProcessor.correctWord}
+                </Typography>
+            </Dialog>Ï
+            <Box display="flex" justifyContent="center" alignItems="center" flexDirection="column" sx={{ marginTop: "4em", fontWeight: "bold" }}>
+                <Typography >
+                    Practice Mode
+                </Typography>
+                <Button variant="outlined" onClick={() => { setRevealWordOpen(true) }} >
+                    Reveal Word
+                </Button>
+            </Box>
+
             <Box sx={styles.main} display="flex" alignItems="center" justifyContent="center">
                 <WordGrid letterGrid={letterGrid} />
             </Box>
@@ -55,4 +68,4 @@ const Main = () => {
     );
 };
 
-export default Main;
+export default Practice;
