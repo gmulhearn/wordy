7c110ccaff91f7209deb0f5a2ed9985e2f119ed0
gmulhearn <immpumpz@gmail.com>
2022-02-01 18:15:56 +1000

    keyboard desktop input

--- src/components/Keyboard.tsx
+++ src/components/Keyboard.tsx
@@ -1,5 +1,5 @@
 import { Box, Button } from '@mui/material';
-import React from 'react';
+import React, { useCallback, useEffect } from 'react';
 
 const styles = {
     letter: {
@@ -17,15 +17,36 @@ const styles = {
 export const ENTER = "ENTER"
 export const DELETE = "DEL"
 
+const VALID_INPUT = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", ENTER, DELETE]
+
 const Keyboard = ({ onKeyboardInput }: { onKeyboardInput: (input: string) => void }) => {
 
     const topRowLetters = ["Q", "W", "E", "R", "T", "Y", "U", "I", "O", "P"]
     const midRowLetters = ["A", "S", "D", "F", "G", "H", "J", "K", "L"]
     const bottomRowLetters = [ENTER, "Z", "X", "C", "V", "B", "N", "M", DELETE]
 
+    const keyPressListener = useCallback((event: KeyboardEvent) => {
+        let letter =
+            event.key == "Backspace" ? DELETE
+                : (event.key == "Enter" ? ENTER
+                    : event.key.toUpperCase())
 
-    return (<Box display="flex" sx={{minWidth:"100%"}} justifyContent="center" flexDirection="column" alignItems="center">
-        <Box display="flex" sx={{minWidth:"100%"}}>
+        if (VALID_INPUT.includes(letter)) {
+            onKeyboardInput(letter)
+        }
+    }, []);
+
+    useEffect(() => {
+        document.addEventListener("keydown", keyPressListener, false);
+
+        return () => {
+            document.removeEventListener("keydown", keyPressListener, false);
+        };
+    }, []);
+
+
+    return (<Box display="flex" sx={{ minWidth: "100%" }} justifyContent="center" flexDirection="column" alignItems="center">
+        <Box display="flex" sx={{ minWidth: "100%" }}>
             {topRowLetters.map((letter) =>
                 <Button sx={styles.letter}
                     variant='contained'
@@ -35,7 +56,7 @@ const Keyboard = ({ onKeyboardInput }: { onKeyboardInput: (input: string) => voi
                 </Button>
             )}
         </Box>
-        <Box display="flex" sx={{minWidth:"100%"}}>
+        <Box display="flex" sx={{ minWidth: "100%" }}>
             {midRowLetters.map((letter) => (
                 <Button sx={styles.letter}
                     variant='contained'
@@ -45,7 +66,7 @@ const Keyboard = ({ onKeyboardInput }: { onKeyboardInput: (input: string) => voi
                 </Button>
             ))}
         </Box>
-        <Box display="flex" sx={{minWidth:"100%"}}>
+        <Box display="flex" sx={{ minWidth: "100%" }}>
             {bottomRowLetters.map((letter) => (
                 <Button sx={styles.letter}
                     variant='contained'
